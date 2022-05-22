d4e7f29b0f9ac59f356937f29432229b6a43c4a0
Robbi <lucas.robbi@hotmail.com>
2022-05-22 18:56:32 -0300

    content and api add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,32 @@
 
+/* Data Table -------------------------------------------------------------------------------------------------- */
+
 document.addEventListener('DOMContentLoaded', function () {
     let table = new DataTable('#myTeams');
 });
+
+
+
+/* API Football ------------------------------------------------------------------------------------------------ */
+
+var myHeaders = new Headers();
+myHeaders.append("x-rapidapi-key", "XxXxXxXxXxXxXxXxXxXxXxXx");
+myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+fetch("https://v3.football.api-sports.io/players/seasons", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "v3.football.api-sports.io",
+        "x-rapidapi-key": "3e00b6948cddd21e1c9d5875a21b03fa"
+    }
+
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.log(err);
+});
+
+
+  
