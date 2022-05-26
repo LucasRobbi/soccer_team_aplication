8c2b4a951ea6c8c41376c3f448ef6396a11e3af2
Robbi <lucas.robbi@hotmail.com>
2022-05-25 22:40:36 -0300

    function aditions and coments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 
+
 /* Data Table -------------------------------------------------------------------------------------------------- */
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -28,6 +29,21 @@ fetch("https://v3.football.api-sports.io/players/seasons", {
 	console.log(err);
 });
 
+/* VALIDATION URL WEBSITE TEAM*/
+
+function is_url(str)
+{
+  regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if (regexp.test(str))
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+}
+
 
 /* DROPZONE PLAYERS*/
 // for (let index = 0; index < array.length; index++) {
@@ -52,6 +68,7 @@ const draggableElement3 = document.querySelector("#myDraggableElement3");
             e.preventDefault();
             dropZone.classList.add("drop-zone-over");
         });
+        
         dropZone.addEventListener("dragleave", e => {
             dropZone.classList.remove("drop-zone-over");
         });
@@ -63,6 +80,23 @@ const draggableElement3 = document.querySelector("#myDraggableElement3");
         
             dropZone.appendChild(droppedElement);
             dropZone.classList.remove("drop-zone-over");
+        });
+    }
+
+    for (const playerDroped of document.querySelectorAll(".playerSearched")){
+        
+        playerDroped.addEventListener("dragleave", e => {
+            playerDroped.classList.add("overrideIni");
+        });
+
+        playerDroped.addEventListener("drop", e => {
+            e.preventDefault();
+            const droppedElementId = e.dataTransfer.getData("text/plain");
+            const droppedElement =  document.getElementById(droppedElementId);
+            playerDroped.classList.add("overrideIni");
+        
+            playerDroped.appendChild(droppedElement);
+            playerDroped.classList.remove("drop-zone-over");
         });
     }
 
